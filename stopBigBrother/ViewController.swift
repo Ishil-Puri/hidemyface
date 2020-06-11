9f7ababb51ab48dde7a8885b70824ef5a859a096
Ishil Puri <ipuri@berkeley.edu>
2020-06-10 17:04:42 -0700

    Initial Commit

---
+++ stopBigBrother/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stopBigBrother
+//
+//  Created by ip on 6/10/20.
+//  Copyright © 2020 The Brotherhood Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
